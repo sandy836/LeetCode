f7854ab41bfcb4e80d1ebb581cc6bf0d7f3ab9e5
sandy836 <sandeepshrivastava518@gmail.com>
2020-04-12 07:12:40 -0700

    30 days challenge sloved

---
+++ 30 Days Challenge/happy_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isHappy(self, n):
+        while (n!=1):
+            _sum = 0
+            while n != 0:
+                digit = n%10
+                _sum += digit**2
+                n = n//10
+            if _sum == 4:
+                return False
+            n = _sum
+        
+        return True
