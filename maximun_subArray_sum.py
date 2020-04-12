fd4e6b317c009a06fa91882340e2ba6541dc05c7
sandy836 <sandeepshrivastava518@gmail.com>
2020-04-12 07:13:09 -0700

    deleted

--- maximun_subArray_sum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        local_max = global_max = nums[0]
-        for i in range(1, len(nums)):
-            local_max = max(local_max+nums[i], nums[i])
-            global_max = max(global_max, local_max)
-        
-        return global_max
--- single_number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        a = 0
-        for i in nums:
-            a ^= i
-        return a
