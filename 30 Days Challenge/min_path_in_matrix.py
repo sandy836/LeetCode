c79b7b1d47bc33f7d74b0d5a48dd997805e5fe08
sandy836 <sandeepshrivastava518@gmail.com>
2020-04-18 10:37:58 -0700

    minimum number of steps

---
+++ 30 Days Challenge/min_path_in_matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        row, col = len(grid), len(grid[0])
+        for i in range(1, col):
+            grid[0][i] += grid[0][i-1]
+        for i in range(1, row):
+            grid[i][0] += grid[i-1][0]
+        for i in range(1, row):
+            for j in range(1, col):
+                grid[i][j] += min(grid[i][j-1], grid[i-1][j])
+        
+        return grid[-1][-1]
